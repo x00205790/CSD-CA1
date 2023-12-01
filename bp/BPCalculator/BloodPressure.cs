669dbb6d0e36039a18fc3fb8659300a6dbb042f3
Craig Dillon <cdillon@heanet.ie>
2023-12-01 15:33:38 +0000

    removing linting for meantime

---
+++ bp/BPCalculator/BloodPressure.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace BPCalculator
+{
+    // BP categories
+    public enum BPCategory
+    {
+        [Display(Name="Low Blood Pressure")] Low,
+        [Display(Name="Ideal Blood Pressure")]  Ideal,
+        [Display(Name="Pre-High Blood Pressure")] PreHigh,
+        [Display(Name ="High Blood Pressure")]  High
+    };
+
+    public class BloodPressure
+    {
+        public const int SystolicMin = 70;
+        public const int SystolicMax = 190;
+        public const int DiastolicMin = 40;
+        public const int DiastolicMax = 100;
+
+        [Range(SystolicMin, SystolicMax, ErrorMessage = "Invalid Systolic Value")]
+        public int Systolic { get; set; }                       // mmHG
+
+        [Range(DiastolicMin, DiastolicMax, ErrorMessage = "Invalid Diastolic Value")]
+        public int Diastolic { get; set; }                      // mmHG
+
+        // calculate BP category
+        public BPCategory Category
+        {
+            get
+            {
+                // implement as part of project
+                //throw new NotImplementedException("not implemented yet");
+                return new BPCategory();                       // replace this
+            }
+        }
+    }
+}
