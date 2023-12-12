af4cea7cf7865447ca8c4b95d374c9e2efd2007a
Craig Dillon <cdillon@heanet.ie>
2023-12-12 13:57:20 +0000

    removing terraform scripts for time being, out of scope and only a nice to have

--- tf-deploy/deploy.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_container_registry" "acr" {
-  name                = "cdillonacr"
-  resource_group_name = var.rg
-  location            = var.location
-  sku                 = "Basic"
-}
