609819aef27cc7e422a45c474eda5e014402fb39
Craig Dillon <cdillon@heanet.ie>
2023-12-16 17:56:57 +0000

    removing unused vars

--- tf-staging/variables.tf
+++ tf-staging/variables.tf
@@ -1,15 +1,9 @@
 ###### I know this is a terrible habit
-variable "app_name" {}
-
 variable "tenant" {}
 
 variable "rg" {}
 
 variable "subscription_id"{}
-
-variable "storage_account_name" {}
-
-variable "storage_container" {}
 
 variable "location" {
   type        = string
